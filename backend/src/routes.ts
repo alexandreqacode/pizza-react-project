9c08c3e9fe85d8d1781ec08aca99be41b73e33da
oliveiraal <alexandre.oliv.junior@gmail.com>
2022-11-26 23:08:35 -0300

    JWT, validação de token, mais controllers

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
-
+import { DetailUserController } from './controllers/user/DetailUserController';
+import { isAuthenticated } from './middlewares/isAuthentucated';
 
 const router = Router();
 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 export{router};
