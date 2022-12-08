7e2d4b6903bd0fa50033d4860251f6205292e5ba
oliveiraal <alexandre.oliv.junior@gmail.com>
2022-12-08 00:20:08 -0300

    quase acabando

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 
-import { create } from "domain";
-import { createContext, ReactNode, useState } from "react";
+//import { create } from "domain";
+import { createContext, ReactNode, useState, useEffect } from "react";
 import {destroyCookie, setCookie, parseCookies} from 'nookies'
 import Router from 'next/router'
 import {api} from '../services/apiClient'
@@ -49,6 +49,29 @@ export function signOut(){
 export function AuthProvider({children}: AuthProviderProps){
     const [user, setUser] = useState<UserProps>();
     const isAuthenticated = !!user;
+
+    useEffect(()=>{
+        
+        //tentar pegar algo no cookie
+        const { '@nextauth': token} = parseCookies();
+
+        if(token){
+            api.get('/me').then(response =>{
+                const {id, name, email} = response.data;
+
+                setUser({
+                    id,
+                    name,
+                    email
+                })
+            })
+            .catch(()=>{
+                //se deu erro faz logout
+                signOut();
+            })
+        }
+
+    }, [])
 
     async function signIn({email, password}: SignInProps){
         try{
